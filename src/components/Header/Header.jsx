5ebfb870b8db5243fa66abd2bc9132c1881826a3
luqmanofficial <lk3281751@gmail.com>
2024-09-30 14:20:59 +0500

    pad header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -57,7 +57,7 @@ const Header = () => {
                   item.url === pathname.hash
                     ? "z-2 lg:text-n-1"
                     : "lg:text-n-1/50"
-                } lg:leading-5 lg:hover:text-n-1 xl:px-6`}
+                } lg:leading-5 lg:hover:text-n-1 xl:px-4`}
               >
                 {item.title}
               </Link>
